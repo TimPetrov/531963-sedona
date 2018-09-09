c3ed99756adb2ed1854039fb5a621b2bed787fe1
TimPetrov <42089837+TimPetrov@users.noreply.github.com>
2018-09-09 16:50:29 +0300

    Финальная сдача 1.1

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,29 @@
     var link = document.querySelector(".button-search");
     var popup = document.querySelector(".modal-hide");
 
+    var form = popup.querySelector(".search-form");
+    var arrival = popup.querySelector("[name=arrival-date]");
+    var departure = popup.querySelector("[name=departure-date]");
+    var children = popup.querySelector("[name=children]");
+    var adult = popup.querySelector("[name=adult]");
+
 
   link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.toggle("modal-hide");
 
 });
+
+    form.addEventListener("submit", function(evt) {
+    if (!arrival.value || !departure.value || !children.value || !adult.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } else {
+      localStorage.setItem("arrival", arrival.value);
+      localStorage.setItem("departure", departure.value);
+      localStorage.setItem("children", children.value);
+      localStorage.setItem("adult", adult.value);
+    }
+  })
